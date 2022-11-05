4f0815e863e47b65639b9243fa6885eab812a439
Nakow-404 <117521297+Nakow-404@users.noreply.github.com>
2022-11-05 13:55:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const yesbtn = document.querySelector('#yesBtn');
+
+yesbtn.addEventListener('click',function () {
+    alert('Ya lo sabia al Zamla')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover',function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+
+    })
